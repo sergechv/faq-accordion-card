f31edffb89c012568f8fa774d5f20eca80540332
Sergio Chaidez <sergioj.chaidez@gmail.com>
2023-10-17 22:09:45 -0600

    I fix it I guess

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const questions = document.querySelectorAll('.question')
+  const questions = document.querySelectorAll('.question');
 
   questions.forEach(question => {
     const toggleAnswer = () => {
-      const answer = question.querySelector('.answer')
-      const arrow = question.querySelector('.arrow')
-      const h2 = question.querySelector('h2')
+      const answer = question.querySelector('.answer');
+      const arrow = question.querySelector('.arrow');
+      const h2 = question.querySelector('h2');
 
-      answer.style.display = (answer.style.display === 'block') ? 'none': 'block'
-      arrow.classList.toggle('rotate')
-      h2.classList.toggle('bold-font')
+      answer.style.display = (answer.style.display === 'block') ? 'none': 'block';
+      arrow.classList.toggle('rotate');
+      h2.classList.toggle('bold-font');
     }
-    question.addEventListener('click', toggleAnswer)
-  })
-})
+    question.addEventListener('click', toggleAnswer);
+  });
+});
